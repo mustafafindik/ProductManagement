0c93ff972d03f707ebaaa0d2eee57575793cd639
Mustafa Fındık <mustafa.findik3@gmail.com>
2021-03-06 09:01:00 +0300

    Entities Added

---
+++ ProductManagement.Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProductManagement.Core.Entities;
+
+namespace ProductManagement.Entities.Concrete
+{
+    public class Product: IEntity
+    {
+        public int Id  { get; set; }
+        public string ProductName { get; set; }
+        public long BarcodeNumber { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Quantity { get; set; }
+    }
+}
---
+++ ProductManagement.Entities/Concrete/ProductImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProductManagement.Core.Entities;
+
+namespace ProductManagement.Entities.Concrete
+{
+    public class ProductImage: IEntity
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
