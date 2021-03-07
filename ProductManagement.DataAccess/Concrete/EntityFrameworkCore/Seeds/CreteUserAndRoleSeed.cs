841571a81545a22a4fe45a3c38b7b7bb29c592be
Mustafa Fındık <mustafa.findik3@gmail.com>
2021-03-07 15:49:52 +0300

    UI Barcode added

--- ProductManagement.DataAccess/Concrete/EntityFrameworkCore/Seeds/CreteUserAndRoleSeed.cs
+++ ProductManagement.DataAccess/Concrete/EntityFrameworkCore/Seeds/CreteUserAndRoleSeed.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using ProductManagement.Core.Entities.Concrete;
 using ProductManagement.Core.Utilities.Security.Hashing;
 using ProductManagement.DataAccess.Concrete.EntityFrameworkCore.Contexts;
+using ProductManagement.Entities.Concrete;
 using ProductManagement.Entities.Dtos;
 
 namespace ProductManagement.DataAccess.Concrete.EntityFrameworkCore.Seeds
@@ -33,11 +34,28 @@ namespace ProductManagement.DataAccess.Concrete.EntityFrameworkCore.Seeds
             if (!context.Users.Any())
             {
                 HashingAndVerifyPasswordHelper.CreatePasswordHash("123123", out var passwordHash, out var passwordKey);
+                var admin = new User
+                {
+                    Email = "admin@admin.com",
+                    FirstName = "Admin",
+                    LastName = "User",
+                    PasswordHash = passwordHash,
+                    PasswordKey = passwordKey,
+                    IsActive = true
+                };
+
+                context.Add(admin);
+                context.SaveChanges();
+
+                var userToRole = new UserRole() { RoleId = roleId, UserId = admin.Id};
+                context.Add(userToRole);
+                context.SaveChanges();
+
                 var user = new User
                 {
-                    Email = "Admin@Admin.com",
-                    FirstName = "Admin",
-                    LastName = "Role",
+                    Email = "user@user.com",
+                    FirstName = "Default",
+                    LastName = "User",
                     PasswordHash = passwordHash,
                     PasswordKey = passwordKey,
                     IsActive = true
@@ -46,10 +64,37 @@ namespace ProductManagement.DataAccess.Concrete.EntityFrameworkCore.Seeds
                 context.Add(user);
                 context.SaveChanges();
 
-                var userToRole = new UserRole() { RoleId = roleId, UserId = user.Id};
-                context.Add(userToRole);
+            }
+
+            if (!context.Products.Any())
+            {
+                var products = new[] {
+                    new Product() { ProductName = "Ürün 1" , BarcodeNumber = 1234567890, Price = 30, Description = "Ürün 1 Açıklaması", Quantity = 12},
+                    new Product() { ProductName = "Ürün 2" , BarcodeNumber = 23423424244, Price = 22, Description = "Ürün 2 Açıklaması", Quantity = 44},
+                    new Product() { ProductName = "Ürün 3" , BarcodeNumber = 2424242424, Price = 66, Description = "Ürün 3 Açıklaması", Quantity = 1},
+                    new Product() { ProductName = "Ürün 4" , BarcodeNumber = 1234244567890, Price = 11, Description = "Ürün 4 Açıklaması", Quantity = 34},
+                    new Product() { ProductName = "Ürün 5" , BarcodeNumber = 124334567890, Price = 33, Description = "Ürün 5 Açıklaması", Quantity = 2},
+                    new Product() { ProductName = "Ürün 6" , BarcodeNumber = 12344567890, Price = 66, Description = "Ürün 6 Açıklaması", Quantity = 27},
+
+                };
+
+                context.AddRange(products);
                 context.SaveChanges();
-             
+
+                var productsimagges = new[] {
+                    new ProductImage() { ProductId = products[0].Id, ImagePath = "Resources/Images/productdefault.png"},
+                    new ProductImage() { ProductId = products[1].Id, ImagePath = "Resources/Images/productdefault.png"},
+                    new ProductImage() { ProductId = products[2].Id, ImagePath = "Resources/Images/productdefault.png"},
+                    new ProductImage() { ProductId = products[3].Id, ImagePath = "Resources/Images/productdefault.png"},
+                    new ProductImage() { ProductId = products[4].Id, ImagePath = "Resources/Images/productdefault.png"},
+                    new ProductImage() { ProductId = products[5].Id, ImagePath = "Resources/Images/productdefault.png"},
+
+
+                };
+
+                context.AddRange(productsimagges);
+                context.SaveChanges();
+
             }
 
 
